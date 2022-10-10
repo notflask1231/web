43213811be3f018b637976a29ea9358961a071ea
Daniel <notflask@gmail.com>
2022-10-10 17:34:53 +0200

    inital commit

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const USERNAME = "notflask";
+export const AVATAR_URL =
+  "https://avatars.githubusercontent.com/u/78703851?v=4";
+export const FIRST_NAME = "Daniel";
+export const SECOND_NAME = "Flask";
+export const DESCRIPTION = [
+  "Hi there 👋🏻 I'm Daniel, I'm 14 years old.",
+  "Self-taught software engineer",
+  "Experienced with Rust and C++ programming languages.",
+];
